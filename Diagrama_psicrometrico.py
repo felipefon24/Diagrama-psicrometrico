f83770453b89ba36d9b6e362b99ed42daecf505a
Felipe Fonseca <felipefonsecabustamante@hotmail.com>
2020-09-11 23:10:03 -0500

    Creacion de la funcion de temperaturam maxima

--- Diagrama_psicrometrico.py
+++ Diagrama_psicrometrico.py
@@ -11,4 +11,23 @@ def Presion_vapor(T):
         B2=3809.4
         C2=226.7
         Ps=exp(A2-(B2/(C2+T)))
+    else:
+        Ps="La temperatura esta fuera de los rangos de operacion"
     return Ps
+
+def Temperatura_maxima(P):
+    A1=23.7093
+    B1=4111
+    C1=237.7
+    T=(B1/(A1-log(P)))
+    if 0<=T<57:
+        Tmax=T
+    elif 57<=T<135:
+        A2=23.186
+        B2=3809.4
+        C2=226.7
+        Tmax=(B2/(A2-log(P)))
+    else:
+        Tmax="La temperatura quedo fuera de los rangos de operacion"
+    return Tmax
+
